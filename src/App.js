65bb69f85f396e0e61e8e447af23820e7a05a289
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2021-03-09 16:46:27 -0700

    fixed component imports

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Hidden from '@material-ui/core/Hidden';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import Navigator from './Navigator';
-import Content from './Content';
-import Header from './Header';
+import Navigator from './components/Navigator';
+import Content from './components/Content';
+import Header from './components/Header';
 
 function Copyright() {
   return (
